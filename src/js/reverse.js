f9a92cd893a7ec1d72c1f9203b806bcd35a82a39
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-05-28 17:20:55 -0400

    reverse

---
+++ src/js/reverse.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {    
+  function reverseToString(number) {
+      const stringedNumber = `${number}`;
+      let reveredString = '';
+      
+      for (const index in stringedNumber) {
+          const char = stringedNumber[index];
+          reveredString = char + reveredString;
+      }
+      
+      return reveredString;
+  }
+  
+  let res;
+  
+  if (x < 0) {
+      const reversed = reverseToString(x * -1);
+      res = Number(reversed) * -1;
+  } else {
+      res = Number(reverseToString(x));
+  }
+  
+  if (res >= (2**31) || res <= ((2**31) * (-1))) return 0;
+  
+  return res;
+};
