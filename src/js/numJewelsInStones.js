bce6854c9b9a1401bc644d3f21b51327baea5e27
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-06-22 13:13:33 -0400

    numJewelsInStones

---
+++ src/js/numJewelsInStones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+  const jewels = new Set(J);
+  let total = 0;
+
+  for (const index in S) {
+      const character = S[index];
+      
+      if (jewels.has(character)) {
+          total = total + 1;
+      }
+  }
+  
+  return total;
+};
