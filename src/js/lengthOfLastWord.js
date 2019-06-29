594e4b46166e7f6b230fa0252889dc533eae2da5
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-06-29 14:44:28 -0400

    update: optimize

--- src/js/lengthOfLastWord.js
+++ src/js/lengthOfLastWord.js
@@ -16,3 +16,36 @@ var lengthOfLastWord = function(s) {
 
   return string.length;
 };
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  let count = 0;
+  let charFound = false;
+  
+  for (let index = s.length - 1; index >= 0; index -= 1) {
+      const currentCharacter = s[index];
+      
+      // current character is not an empty space
+      // and a character has been indexed
+      if (currentCharacter !== ' ' && charFound) {
+          count += 1;
+      } 
+      // current character is not an empty space 
+      // and it's out first encounter with one
+      if (currentCharacter !== ' ' && !charFound) {
+          charFound = true;
+          count += 1;
+      }
+      // current character is an empty space
+      // and a character has been indexed
+      else if (currentCharacter === ' ' && charFound) {
+          index = -1;
+      }
+  }
+  
+  return count;
+};
