6646240defc3c13d7f88fb0d4928eeb9a91c4ac5
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-11 10:57:03 -0400

    strStr fmt

--- src/js/strStr.js
+++ src/js/strStr.js
@@ -3,7 +3,7 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
+var strStr = function (haystack, needle) {
   return haystack.indexOf(needle);
 };
 
@@ -12,32 +12,32 @@ var strStr = function(haystack, needle) {
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
+var strStr = function (haystack, needle) {
   if (haystack.length + needle.length === 0 || needle === '') {
-      return 0;
+    return 0;
   }
-  
+
   let matching = false;
   let checkIndex = 0;
   let indexStart = 0;
-  
-  for (let i = 0, len = haystack.length; i < len && needle[checkIndex]; i += 1) {
-      const current = haystack[i];
-      
-      if (current === needle[checkIndex]) {
-          if (!matching) {
-              indexStart = i;
-              matching = true;
-          }
-          checkIndex += 1;
-      } else {
-          if (matching) {
-            i = indexStart;
-          }
 
-          matching = false;
-          checkIndex = 0;
+  for (let i = 0, len = haystack.length; i < len && needle[checkIndex]; i += 1) {
+    const current = haystack[i];
+
+    if (current === needle[checkIndex]) {
+      if (!matching) {
+        indexStart = i;
+        matching = true;
       }
+      checkIndex += 1;
+    } else {
+      if (matching) {
+        i = indexStart;
+      }
+
+      matching = false;
+      checkIndex = 0;
+    }
   }
 
   return (matching && checkIndex === needle.length) ? indexStart : -1;
