58f955c566bc28f58b5a2c1ceeccacd74dc79c79
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-11 10:56:25 -0400

    strStr

---
+++ src/js/strStr.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle);
+};
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  if (haystack.length + needle.length === 0 || needle === '') {
+      return 0;
+  }
+  
+  let matching = false;
+  let checkIndex = 0;
+  let indexStart = 0;
+  
+  for (let i = 0, len = haystack.length; i < len && needle[checkIndex]; i += 1) {
+      const current = haystack[i];
+      
+      if (current === needle[checkIndex]) {
+          if (!matching) {
+              indexStart = i;
+              matching = true;
+          }
+          checkIndex += 1;
+      } else {
+          if (matching) {
+            i = indexStart;
+          }
+
+          matching = false;
+          checkIndex = 0;
+      }
+  }
+
+  return (matching && checkIndex === needle.length) ? indexStart : -1;
+};
