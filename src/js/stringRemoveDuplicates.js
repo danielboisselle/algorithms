4c008562149a0cd119401af22b3bff03e5a003b9
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-29 10:54:05 -0400

    stringRemoveDuplicates

---
+++ src/js/stringRemoveDuplicates.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+  let first = 0;
+  let second = 1;
+  
+  while (s[second]) {
+      if (s[first] === s[second]) {
+          s = s.slice(0, first) + s.slice(second + 1);
+          first = 0;
+          second = 1;
+      } else {
+          first += 1;
+          second += 1;
+      }
+  }
+  
+  return s
+};
