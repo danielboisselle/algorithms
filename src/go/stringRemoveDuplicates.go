be0792e08797ee58ebabdd1d240e4405c7cdf3d7
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-29 10:54:24 -0400

    stringRemoveDuplicates

---
+++ src/go/stringRemoveDuplicates.go
@@ -0,0 +1,14 @@
+func removeDuplicates(S string) string {
+	i := 0
+	
+	for i < len(S) - 1 {
+			if S[i] == S[i + 1] {
+				S = S[0: i] + S[i + 2:]
+				i = 0
+			} else {
+				i += 1
+			}
+	}
+
+	return S
+}
