de018d5c6b6db48ea0314ffda8509bc1568d0516
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-08-03 21:44:01 -0400

    isCousins

---
+++ src/js/isCousins.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+var isCousins = function(_root, x, y) {
+  let found = {};
+  
+  let find = (root, parent, depth = 0) => {
+      if (found.x && found.y) return;
+      
+      if (root.left) {
+          find(root.left, root.val, depth + 1);
+      }
+      
+      if (root.right) {
+          find(root.right, root.val, depth + 1);
+      }
+      
+      if (root.val === x) {
+          found.x = depth;
+          found.xParent = parent;
+      }
+      
+      if (root.val === y) {
+          found.y = depth;
+          found.yParent = parent;
+      }
+  }
+  
+  find(_root)
+  
+  return found.x === found.y && found.xParent !== found.yParent;
+};
