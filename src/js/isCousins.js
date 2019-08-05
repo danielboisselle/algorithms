8c1423fbe88ee836b2b9ea18b527a7435a4dea5c
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-08-04 22:39:12 -0400

    fmt

--- src/js/isCousins.js
+++ src/js/isCousins.js
@@ -11,32 +11,32 @@
  * @param {number} y
  * @return {boolean}
  */
-var isCousins = function(_root, x, y) {
+var isCousins = function (_root, x, y) {
   let found = {};
-  
+
   let find = (root, parent, depth = 0) => {
-      if (found.x && found.y) return;
-      
-      if (root.left) {
-          find(root.left, root.val, depth + 1);
-      }
-      
-      if (root.right) {
-          find(root.right, root.val, depth + 1);
-      }
-      
-      if (root.val === x) {
-          found.x = depth;
-          found.xParent = parent;
-      }
-      
-      if (root.val === y) {
-          found.y = depth;
-          found.yParent = parent;
-      }
+    if (found.x && found.y) return;
+
+    if (root.left) {
+      find(root.left, root.val, depth + 1);
+    }
+
+    if (root.right) {
+      find(root.right, root.val, depth + 1);
+    }
+
+    if (root.val === x) {
+      found.x = depth;
+      found.xParent = parent;
+    }
+
+    if (root.val === y) {
+      found.y = depth;
+      found.yParent = parent;
+    }
   }
-  
+
   find(_root)
-  
+
   return found.x === found.y && found.xParent !== found.yParent;
 };
