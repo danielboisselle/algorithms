a7cf485510eed003fb2bbb169656a712cb4b1929
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-08-09 18:33:35 -0400

    isPerfectSquare

---
+++ src/js/isPerfectSquare.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+  let check = num;
+  
+  while (check > 0) {
+      if (check * check === num) {
+          return true;
+      }
+      
+      check -= 1;
+  }
+  
+  return !!check;
+};
