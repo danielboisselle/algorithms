8e3f05efa2acd9731af9520dca08cd5e929cb59b
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-09-16 17:46:13 -0400

    flatten reducer

--- src/ts/flatten.ts
+++ src/ts/flatten.ts
@@ -1,13 +1,11 @@
 const flatten = (arr: any[]): number[] => {
-  const flattened: number[] = [];
-
-  for (const iterator of arr) {
+  return arr.reduce((flattened: number[], iterator) => {
     if (Array.isArray(iterator)) {
       flattened.push(...flatten(iterator));
     } else if (typeof iterator === 'number') {
       flattened.push(iterator);
     }
-  }
 
-  return flattened;
+    return flattened;
+  }, []);
 };
